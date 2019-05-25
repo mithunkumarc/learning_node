030790a55aa91db63b8d513b3e50a0d700c3e4c3
mithunkumarc <mithunchannappa@gmail.com>
2019-05-25 16:06:24 +0530

    Delete 1. helloworld.js

--- examples/tkmagesh/1. helloworld.js
+++
@@ -1,6 +0,0 @@
-// make sure you have installed node and npm
-// open terminal , open directory of js file
-// command to run : node helloworld.js
-// check node version : node -v
-
-console.log('welcome to Node.js');
