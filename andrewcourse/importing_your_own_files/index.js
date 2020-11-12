b52eb44b4bae21f81254a1ebba7710feb35052f8
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-11-12 22:09:59 +0530

    Add files via upload

---
+++ andrewcourse/importing_your_own_files/index.js
@@ -0,0 +1,3 @@
+//no need to give .js extension
+const getNotes = require('./notes');
+console.log(getNotes());
---
+++ andrewcourse/importing_your_own_files/notes.js
@@ -0,0 +1,6 @@
+const getNotes = () => {
+	return "This is my notes."
+}
+
+// you are exporting only one function, getNotes. You can export multiple function, variables too.
+module.exports = getNotes
