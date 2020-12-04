ba17234a3a0f8952cf24b11aa5e324ae55ad2049
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-12-04 22:36:43 +0530

    Create twoReqWithSameURL.js

---
+++ expressjs/requestMethodCallback/twoReqWithSameURL.js
@@ -0,0 +1,18 @@
+/*
+  if two request method has same url, first one will get executed first
+*/
+
+const express = require('express')
+const app = express()
+
+
+app.get("/hello", (req, res, next) => {
+	console.log("first"); // this will be printed first
+	next(); // request will handover to next request method with same url
+});
+
+app.get("/hello", (req, res) => {
+	console.log("second");  // this will be printed second
+});
+
+app.listen(3000);
