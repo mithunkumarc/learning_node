a5d92b48c1171eda60616c0aa80dbac7b3da3076
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-12-07 12:41:11 +0530

    Update index.js

--- expressjs/download_file/download_file/index.js
+++ expressjs/download_file/download_file/index.js
@@ -8,6 +8,8 @@ app.get('/statement', (req, res, next) => {
 	
 	//res.sendFile(): this will make browser to load/render file
 	//to make browser to download use below
+	// create bankdocuments folder and put a dummy file BankStatementChequing
+	// downloading name johnsBankStatement
 	res.download(path.join(__dirname,'bankdocuments/BankStatementChequing.png'),"johnsBankStatement.png", (err) => {
 		// what if file doesn't exist/generated
 		console.log("error occured : ",err);
