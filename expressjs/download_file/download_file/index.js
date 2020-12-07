bde9a70e6a843895daba23f73e6d39a2aadf7383
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-12-07 12:38:29 +0530

    Add files via upload

---
+++ expressjs/download_file/download_file/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+// dont expose(express export) user document like
+// static file html/css etc
+app.get('/statement', (req, res, next) => {
+	
+	//res.sendFile(): this will make browser to load/render file
+	//to make browser to download use below
+	res.download(path.join(__dirname,'bankdocuments/BankStatementChequing.png'),"johnsBankStatement.png", (err) => {
+		// what if file doesn't exist/generated
+		console.log("error occured : ",err);
+	});
+	console.log("headers kalsidya ", res.headersSent);
+})
+
+
+app.listen(3000);
