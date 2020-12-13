ea378b2ef2e35e694c0bba7390985b356c7430ef
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2020-12-13 10:40:24 +0530

    Update delete_session.js

--- expressjs/express-session/delete_session.js
+++ expressjs/express-session/delete_session.js
@@ -1,1 +1,60 @@
+/*
+  index.js
+  npx nodemon index.js
+  
+  steps to test:
+  1. set session data
+  localhost:3000/name/rahul
+
+  2. read session data
+  localhost:3000/home_page
+  
+  3. delete session data
+  localhost:3000/logout
+
+  4. test again whether session data is deleted
+  localhost:3000/home_page
+  it should return "hello user"
+*/
+
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+app.use(cookieParser())
+
+app.use(session({
+	secret: "this is for encrypting data",
+	resave: false,
+	saveUninitialized: false
+}));
+
+
+// localhost:3000/name/rahul
+app.get('/name/:username', (req, res) => {
+	console.log(req.params.username);
+	req.session['username'] = req.params.username;
+	res.send(req.params);
+})
+
+// localhost:3000/home_page
+app.get("/home_page", (req, res) => {
+	if(!req.session.username) {
+		req.session.username = 'user';
+	}
+	username = req.session.username;
+	res.send(`hello, ${req.session.username}`);
+})
+
+// destroy session
+// localhost:3000/logout
+app.get("/logout", (req, res) => {
+	req.session.destroy((err) => {
+		console.log("logged out");
+	});
+	res.send("logged out");
+})
+
+app.listen(3000);
 
